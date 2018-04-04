eb8cfd1c37f05794f4be85ab8e1cfb5101fb528a
bjlag <vlad.duplin@ya.ru>
2018-04-04 12:41:20 +0300

    Добавил ссылку в Product

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -41,6 +41,7 @@ AppAsset::register($this);
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Test', 'url' => ['/test/index']],
+            ['label' => 'Product', 'url' => ['/product/index']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]

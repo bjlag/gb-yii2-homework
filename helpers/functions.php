a9f8eed0512fce24d8af1056e29cf7dfeca79589
bjlag <vlad.duplin@ya.ru>
2018-04-11 18:24:16 +0300

    shortcut

---
+++ helpers/functions.php
@@ -0,0 +1,19 @@
+<?php
+function _log( $data )
+{
+    \Yii::info( \yii\helpers\VarDumper::dumpAsString( $data, 5 ), '_' );
+}
+
+function _end( $data )
+{
+    echo \yii\helpers\VarDumper::dumpAsString( $data, 5, true );
+    exit();
+}
+
+/**
+ * @return \yii\console\Application | \yii\web\Application | app\components\Application
+ */
+function app()
+{
+    return \Yii::$app;
+}

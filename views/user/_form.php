5eb5dc2c002ecbe74f47a1fee3f5e2903b947038
bjlag <vlad.duplin@ya.ru>
2018-04-18 18:35:08 +0300

    Удалил вывод полей вывода даты создания и обновления пользователя

--- views/user/_form.php
+++ views/user/_form.php
@@ -24,10 +24,6 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'auth_key')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <?= $form->field($model, 'updated_at')->textInput() ?>
-
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>

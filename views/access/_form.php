55f5ada8db909518ab785f346edc04149168e17d
bjlag <vlad.duplin@ya.ru>
2018-04-21 22:50:48 +0300

    Удалил лишние поля в форме, выбор пользователя из выпадающего списка

--- views/access/_form.php
+++ views/access/_form.php
@@ -6,15 +6,14 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $model app\models\Access */
 /* @var $form yii\widgets\ActiveForm */
+/* @var $users array */
 ?>
 
 <div class="access-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'note_id')->textInput() ?>
-
-    <?= $form->field($model, 'user_id')->textInput() ?>
+    <?= $form->field($model, 'user_id')->dropDownList( $users ) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
--- views/access/create.php
+++ views/access/create.php
@@ -5,6 +5,7 @@ use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Access */
+/* @var $users array */
 
 $this->title = 'Create Access';
 $this->params['breadcrumbs'][] = ['label' => 'Accesses', 'url' => ['index']];
@@ -16,6 +17,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?= $this->render('_form', [
         'model' => $model,
+        'users' => $users
     ]) ?>
 
 </div>
